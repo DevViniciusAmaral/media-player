83493c66ee47a22db3f0b7c5f888a1919c943d58
DevViniciusAmaral <viniciusamaraldev@gmail.com>
2025-06-19 16:04:51 -0300

    refactor: removido expo-font e adicionado fonte nativamente

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,26 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
 
 export const App = () => {
-  const [loaded, error] = useFonts({
-    "Satoshi-Light": require("../../assets/fonts/Satoshi-Light.otf"),
-    "Satoshi-Regular": require("../../assets/fonts/Satoshi-Regular.otf"),
-    "Satoshi-Medium": require("../../assets/fonts/Satoshi-Medium.otf"),
-    "Satoshi-Bold": require("../../assets/fonts/Satoshi-Bold.otf"),
-    "Satoshi-Black": require("../../assets/fonts/Satoshi-Black.otf"),
-  });
-
-  if (!loaded && !error) return null;
-
-  useEffect(() => {
-    if (loaded || error) SplashScreen.hideAsync();
-  }, [loaded, error]);
-
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
